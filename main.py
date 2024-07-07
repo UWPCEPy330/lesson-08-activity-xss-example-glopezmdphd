3ec8075d288fec9292cdd56d759789c30107e8cf
George A Lopez MD PhD <glopezmdphd@gmail.com>
2024-07-06 19:27:08 -0700

    Sanitize input to prevent XSS

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
-import base64
 import html
 from flask import Flask, request
-from model import Message 
+from model import Message
 
 app = Flask(__name__)
 
@@ -42,9 +41,10 @@ def home():
 </body>
 </html>
 """
-    return body 
+    return body
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
+
 
